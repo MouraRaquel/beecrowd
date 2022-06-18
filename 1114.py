f9e9d56921f4d16460a9a00f90cc9593938c3709
MouraRaquel <raquelaguiar@id.uff.br>
2022-06-18 08:47:38 -0300

    challenges

---
+++ 1114.py
@@ -0,0 +1,7 @@
+senha = int(input())
+
+while senha != 2002:
+    print('Senha Invalida')
+    senha = int(input())
+if senha == 2002:
+    print('Acesso Permitido')
---
+++ 1930.py
@@ -0,0 +1,5 @@
+t1, t2, t3, t4 = map(int, input().split())
+
+ti = (t1 + t2 + t3 + t4)-3
+
+print(ti)
