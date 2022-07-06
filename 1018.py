a592f3efc1b9017c8d28787451d34e1e82a44cf4
MouraRaquel <raquelaguiar@id.uff.br>
2022-07-05 22:34:33 -0300

    challenges

--- 1018.py
+++ 1018.py
@@ -1,18 +1,9 @@
 n = int(input())
 
-cem = n//100
-cinquenta = (n%100)//50
-vinte = ((n%100)%50)//20
-dez = ((n%50)%20)//10
-cinco = ((n%20)%10)//5
-dois = ((n%10)%5)//2
-um = ((n%5)%2)//1
+notas = [100, 50, 20, 10, 5, 2, 1]
 
 print(n)
-print(cem, 'nota(s) de R$ 100,00')
-print(cinquenta, 'nota(s) de R$ 50,00')
-print(vinte, 'nota(s) de R$ 20,00')
-print(dez, 'nota(s) de R$ 10,00')
-print(cinco, 'nota(s) de R$ 5,00')
-print(dois, 'nota(s) de R$ 2,00')
-print(um, 'nota(s) de R$ 1,00')
+for i in notas:
+    count = n // i
+    print('{:.0f} nota(s) de R$ {},00'.format(count, i))
+    n %= i
